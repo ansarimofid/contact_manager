6510f5bccdd41c272bb0ab33c023f67d53519f02
Mofid Ansari <ansarimofid@gmail.com>
2015-07-17 03:24:05 +0530

    updated

--- fetch.data.php
+++ fetch.data.php
@@ -1,24 +1,72 @@
 <?php
-$server="localhost";
-$user="mofid";
-$password="";
-$database="contacts";
-$conn=new mysqli($server, $user, $password, $database);
+//Create Connection
+function create_conn($server, $user, $password, $db = "none") {
+    if($db=="none"){
+        return new mysqli($server, $user, $password);
+    }
+    else{
+        return new mysqli($server, $user, $password,$db);
+    }
 
-if ($conn->connect_error) {
-    echo $conn->connect_error;
+    if ($conn->connect_error) {
+    echo "Unable to Create Connection:".$conn->connect_error;
+    }
+}
+//Create Database
+function create_db($conn,$dbname){
+    $query="CREATE DATABASE ".$dbname;
+    if(sql_query($conn,$query)){
+        echo "Successfully Created Database";
+    }
 }
 
-$sql="insert into list (fname,lname) values('Kanju','ansari')";
-sql_query($sql);
-
-
-
-
-function sql_query($sql){
-    if($GLOBALS['conn']->query($sql)===TRUE){
-    echo "Success";
+//Delete Database
+function delete_db($conn,$dbname){
+    $query="DROP DATABASE ".$dbname;
+    if(sql_query($conn,$query)){
+        echo "Successfully Deleted Database";
+    }
 }
-else
-    echo $GLOBALS['conn']->error;
+
+//Create Table
+function create_tb($conn,$tbname,$col){
+    $query="CREATE TABLE ".$tbname."(".$col.")";
+    if(sql_query($conn,$query)){
+        echo "Successfully Created Table";
+    }
 }
+
+//Delete Table
+function delete_tb($conn,$tbname){
+    $query="DROP TABLE ".$tbname;
+    if(sql_query($conn,$query)){
+        echo "Successfully Deleted Table";
+    }
+}
+
+//Query Database
+function sql_query($conn,$query) {
+    if (!$conn->query($query)) {
+        echo "Error: ".$conn->error;
+        return 0;
+    }
+    return 1;
+}
+
+//Fetch Data from Database
+function sql_fetch($conn,$query){
+    if ($result = $conn->query($query))
+    return $result;
+    else return 0;
+}
+
+$conn=new mysqli("localhost","mofid","","testing");
+/*create_tb($conn,
+          "test",
+          "id INT AUTO_INCREMENT PRIMARY KEY,fname varchar(30) NOT NULL,lname varchar(30) NOT NULL ,Gender CHAR(1) NOT NULL,bday DATE NOT NULL");*/
+
+$sql='insert into test (fname,lname,bday,gender) values("Mofid","Ansari","1996-02-25","M")';
+
+sql_query($conn,$sql);
+
+?>
--- index.php
+++ index.php
@@ -1,17 +1,39 @@
 <!doctype html>
 <html>
-    <head>
-        <title>Document</title>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
 
-        <form action="fetch.data.php">
-            Name:<input type="text">
-            <input type="submit">
-        </form>
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="/contact_manager/assets/css/style.css">
+</head>
 
-        <script src="js/jquery.min.js"></script>
-        <script src="js/main.js"></script>
-    </body>
+<body>
+
+    <form action="fetch.data.php" method="post">
+        <div>
+            <label for="fname">First Name:</label>
+            <input type="text" name="fname">
+        </div>
+        <div>
+            <label for="lname">Last Name:</label>
+            <input type="text" name="lname">
+        </div>
+        <div>
+            <label for="gender">Gender:</label>
+            <input type="text" name="gender">
+        </div>
+        <div>
+            <label for="bday">Date of Birth:</label>
+            <input type="date" name="bday">
+        </div>
+
+        <div>
+            <button type="submit">Add Contact</button>
+        </div>
+
+    </form>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
 </html>
