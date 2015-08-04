cca200aa6f0d20b02d8d1979eb7e12892cda5d77
Mofid Ansari <ansarimofid@gmail.com>
2015-08-05 00:48:29 +0530

    Added Navigation

--- index.php
+++ index.php
@@ -3,16 +3,64 @@
 
 <head>
     <title>Document</title>
-    <link rel="stylesheet" href="/contact_manager/assets/css/style.css">
     <link rel="stylesheet" href="/contact_manager/assets/css/bootstrap.css">
+    <link rel="stylesheet" href="/contact_manager/assets/css/style.css">
+    <link rel="stylesheet" href="/contact_manager/assets/css/font-awesome.css">
 </head>
 
 <body>
-<!--   Main Container-->
-    <div class="wrapper">
+    <!--   Main Container-->
+    <div class="wrapper container-fluid">
+        <header class="tes100">
+            <div class="container-fluid">
+                <nav class="navbar navbar-default nav-log">
+                    <div class="container-fluid">
+                        <div class="collapse navbar-collapse">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="">Login
+                                <i class="fa fa-sign-in fa-2x"></i>
+                                </a>
+                                </li>
+                                <li><a href="">Signup</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div class="brand">
+                    <div class="brand-icon">
+                        <div>
+                            CM
+                        </div>
+                    </div>
+                </div>
+                <nav class="navbar navbar-default nav-main">
+                    <div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-content">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                       </div>
 
+                        <div class="collapse navbar-collapse" id="nav-content">
+                            <ul class="nav navbar-nav navbar-center">
+                                <li><a href="">Home</a></li>
+                                <li><a href="">View All</a></li>
+                                <li><a href="">Import</a></li>
+                                <li><a href="">Search</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+            </div>
+        </header>
     </div>
-<!--Scripts and Plugins-->
+    <!--Scripts and Plugins-->
     <script src="/contact_manager/assets/js/jquery.min.js"></script>
     <script src="/contact_manager/assets/js/main.js"></script>
     <script src="/contact_manager/assets/js/bootstrap.min.js"></script>
